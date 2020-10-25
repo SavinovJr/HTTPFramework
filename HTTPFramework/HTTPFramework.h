f8e08e11b42f6f424b6b757c6e1770d5681afb47
Anton Savinov <Anton.Savinov@Harman.com>
2020-10-25 15:33:02 +0300

    Initial Commit

---
+++ HTTPFramework/HTTPFramework.h
@@ -0,0 +1,18 @@
+//
+//  HTTPFramework.h
+//  HTTPFramework
+//
+//  Created by Антон Савинов on 25.10.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTTPFramework.
+FOUNDATION_EXPORT double HTTPFrameworkVersionNumber;
+
+//! Project version string for HTTPFramework.
+FOUNDATION_EXPORT const unsigned char HTTPFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPFramework/PublicHeader.h>
+
+
