dedd08a68252919fc0f985cebf1ccd61cc98ae4c
Anton Savinov <anton-savinov@mail.ru>
2020-11-02 13:32:23 +0300

    fixed swiftlint issues

--- HTTPFramework/HTTPBody.swift
+++ HTTPFramework/HTTPBody.swift
@@ -13,7 +13,7 @@ public protocol HTTPBody {
     // we’d like a way to specify the Content-Type: application/json header automatically,
     // without also having to manually specify this on the request.
     var additionalHeaders: [String: String] { get }
-    
+
     func encodeData() throws -> Data
 }
 
@@ -24,49 +24,49 @@ extension HTTPBody {
 
 public struct EmptyBody: HTTPBody {
     public var isEmpty: Bool = true
-    
+
     public func encodeData() throws -> Data {
         Data()
     }
 }
 
 public struct DataBody: HTTPBody {
-    
+
     private let data: Data
-    
-    public var additionalHeaders: [String : String]
-    
+
+    public var additionalHeaders: [String: String]
+
     public var isEmpty: Bool {
         data.isEmpty
     }
-    
+
     init(data: Data, additionalHeaders: [String: String] = [:]) {
         self.data = data
         self.additionalHeaders = additionalHeaders
     }
-    
+
     public func encodeData() throws -> Data {
         data
     }
 }
 
 public struct JSONBody<T: Encodable>: HTTPBody {
-    
+
     private let value: T
     private let encoder = JSONEncoder()
-    
+
     public var isEmpty: Bool {
         // an empty string encodes to a non-empty JSON value
         false
     }
-    public var additionalHeaders: [String : String] = [
+    public var additionalHeaders: [String: String] = [
         "Content-Type": "application/json; charset=utf-8"
     ]
-    
+
     init(value: T) {
         self.value = value
     }
-    
+
     public func encodeData() throws -> Data {
         try encoder.encode(value)
     }
@@ -74,19 +74,19 @@ public struct JSONBody<T: Encodable>: HTTPBody {
 
 public struct FormBody: HTTPBody {
     private let queryItems: [URLQueryItem]
-    
+
     public var isEmpty: Bool {
         queryItems.isEmpty
     }
-    
-    public var additionalHeaders: [String : String] = [
+
+    public var additionalHeaders: [String: String] = [
         "Content-Type": "application/x-www-form-urlencoded; charset=utf-8"
     ]
-    
+
     init(queryItems: [URLQueryItem]) {
         self.queryItems = queryItems
     }
-    
+
     public func encodeData() throws -> Data {
         let pieces = queryItems.map { (queryItem) -> String in
             let name = urlEncode(queryItem.name)
